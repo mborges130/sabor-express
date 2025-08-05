686ebab4cb3ed031dea0ffab008c4a1d4d46d900
Mateus Borges <mateusdantas73@icloud.com>
2025-08-05 14:20:47 -0300

    Adicionando funções para cadastrar e listar restaurantes na API Sabor Express

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import os
 
+restaurantes = ['Pizzaria do Mateus', 'Sushi do João', 'Churrascaria do Carlos']
+endereco = ['Rua das Flores, 123', 'Avenida Brasil, 456', 'Praça Central, 789']
+telefone = [111, 2222, 3333]
+
 def exibir_nome_do_programa():
 
     print('Sabor Express - API de pedidos de comida\n')
@@ -20,14 +24,38 @@ def opcao_invalida():
     input('Pressione Enter para continuar...')
     main()
 
+def cadastrar_novo_restaurante():
+    os.system('cls')
+    print('Cadastrar novo restaurante\n')
+    nome = input('Nome do restaurante: ')
+    restaurantes.append(nome)
+    endereco = input('Endereço do restaurante: ')
+    restaurantes.append(endereco)
+    telefone = input('Telefone do restaurante: ')
+    restaurantes.append(telefone)
+    print(f'\nRestaurante {nome} cadastrado com sucesso!\n')
+    input('Pressione Enter para continuar...')
+    main()
+
+def listar_restaurantes():
+    os.system('cls')
+    print('Listar Restaurantes\n')
+    if not restaurantes:
+        print('Nenhum restaurante cadastrado.\n')
+    else:
+        for i, restaurante in enumerate(restaurantes):
+            print(f'{i + 1}. {restaurante} - Endereço: {endereco} - Telefone: {telefone[i]}')
+    input('Pressione Enter para continuar...')
+    main()
+
 def escolher_opcao():
     try:
         opcao_escolhida = int(input('Escolha uma opção: ')) 
         print(f'Você escolheu a opção: {opcao_escolhida} ')
         if opcao_escolhida == 1:
-            print('Cadastrar Restaurante') 
+            cadastrar_novo_restaurante()
         elif opcao_escolhida == 2:
-            print('Listar Restaurantes')
+            listar_restaurantes()
         elif opcao_escolhida == 3:
             print('Ativar Restaurante') 
         elif opcao_escolhida == 4:
