0cf6f455752eb95ec90ca79081db5b32724e8c16
Mateus Borges <mateusdantas73@icloud.com>
2025-08-05 13:53:51 -0300

    Refatorando a estrutura do programa encapsulando instruções de impressão em funções para melhor organização e legibilidade

--- app.py
+++ app.py
@@ -1,32 +1,49 @@
 import os
 
-print('Sabor Express - API de pedidos de comida\n')
+def exibir_nome_do_programa():
 
-print('1. Cadastar Restaurante ')
-print('2. Listar Restaurantes')
-print('3. Ativar restaurante')
-print('4. sair\n')
+    print('Sabor Express - API de pedidos de comida\n')
+def exibir_opcoes():
+    print('1. Cadastar Restaurante ')
+    print('2. Listar Restaurantes')
+    print('3. Ativar restaurante')
+    print('4. sair\n')
 
-
-
-opcao_escolhida = int(input('Escolha uma opção: ')) 
 
 def finalizar_app():
     os.system('cls')
     print('Obrigado por usar o Sabor Express!')
     exit()
+    
+def opcao_invalida():
+    print('Opção inválida, tente novamente.\n')
+    input('Pressione Enter para continuar...')
+    main()
 
-print(f'Você escolheu a opção: {opcao_escolhida} ')
+def escolher_opcao():
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: ')) 
+        print(f'Você escolheu a opção: {opcao_escolhida} ')
+        if opcao_escolhida == 1:
+            print('Cadastrar Restaurante') 
+        elif opcao_escolhida == 2:
+            print('Listar Restaurantes')
+        elif opcao_escolhida == 3:
+            print('Ativar Restaurante') 
+        elif opcao_escolhida == 4:
+            finalizar_app()    
+        else: 
+            opcao_invalida()
+    except ValueError:
+        opcao_invalida()
+    
 
-if opcao_escolhida == 1:
-    print('Cadastrar Restaurante') 
-elif opcao_escolhida == 2:
-    print('Listar Restaurantes')
-elif opcao_escolhida == 3:
-    print('Ativar Restaurante') 
-else : 
-    finalizar_app()
+def main():
+    os.system('cls')
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
 
 
-
-
+if __name__ == '__main__':
+    main()
